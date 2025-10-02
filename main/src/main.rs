60e68e03826bb99fea483d98fa67ac86491b3d2f
Diego-Diaz797 <ddiaz11@elon.edu>
2025-10-01 20:31:22 -0400

    checking how to insert a varibal inside a print statement.

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let money = 10;
+    let money_lost = -5;
+    let money_won = 5;
+    println!("Blackjack!!");
+    println!("money: {}", money);
+    println!("{}", money);
+    println!("lost: {money_lost},won:  {money_won},total:  {money}");
 }
