ef87009ed4f4fd7af9e9b18a9f18346c00467f27
Diego-Diaz797 <ddiaz11@elon.edu>
2025-10-01 20:53:44 -0400

    making a function with a return statement. the way to return something in rust you have to call the var again like you are initalzing it. using let, to help creat the and assign a var again

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     let money_won = 5;
     println!("Blackjack!!");
     println!("money: {}", money);
-    println!("{}", money);
-    println!("lost: {money_lost},won:  {money_won},total:  {money}");
+    let money = check_money(money, money_lost);
+    println!("money: {}", money);
+    let money = check_money(money, money_won);
+    println!("money: {}", money);
+}
+
+//checking how much money is gained or lost
+fn check_money(original_money: i32,changed_money: i32) -> i32{
+    if changed_money < 0{ //losing money
+        let total_money = original_money + changed_money;
+        total_money
+    }
+    else{ //gaining money
+        let total_money = original_money + changed_money;
+        total_money
+    }
 }
